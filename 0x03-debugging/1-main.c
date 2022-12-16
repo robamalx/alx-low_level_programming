e0d4f06ffb960798d62f406a031940c36c28aab3
robamalx <robinsonslmn@gmail.com>
2022-12-16 02:36:17 -0800

    ff

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinant loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinsnt loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i - 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinant loop avoided! \\o/\n");
+
+	return (0);
+}
