32591f7bfc59de2671fc737afb0ccfbeaba86168
robamalx <robinsonslmn@gmail.com>
2022-12-19 08:55:22 -0800

    tory

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
- * @s: staring to beprinted
+ * rev_string - prints a string in reverse
+ * @s: input
+ * Return: string in reverse
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
-	int i = 0;
+	char rv = s[0];
+	int c = 0;
+	int i;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c++;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
 }
