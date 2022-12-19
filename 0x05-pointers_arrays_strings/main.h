3de8edcff6a9176d76db1ba48b4698bacb9a2d72
robamalx <robinsonslmn@gmail.com>
2022-12-19 03:05:19 -0800

    star boy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,14 +3,13 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-void _strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dust, char *src);
-int _putchar(char c);
+char *_strcpy(char *dest, char *src);
 
 #endif
