a086dfd18f94fa3ecd69dcfcea6371628b0c6913
robamalx <robinsonslmn@gmail.com>
2022-12-19 03:31:20 -0800

    50

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the vakue of n to 98
- *@n: input
+ * reset_to_98 - a function that takes a pointer to in intas peremeter
+ * and updates the value it points to to 98
+ * @n: input
  * Return: n
  */
-void rest_to_98(int *n)
+
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
